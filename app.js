44f14fbb35d38ca4c3e7bbf5828552ed1f2c6535
Polvere243 <danielercoli@gmail.com>
2024-10-21 12:18:05 +0200

    created a variable for the deadline

--- app.js
+++ app.js
@@ -27,3 +27,7 @@ const weekdays = [
 const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
+
+// dichiaro una variabile per stabilire la data di scadenza
+
+let futureDate = new Date(2024, 4, 24, 11, 30, 0);
