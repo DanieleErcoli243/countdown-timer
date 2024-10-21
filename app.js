eb252d55c1a24227829bf6bd1d4545a00cb0b4f4
Polvere243 <danielercoli@gmail.com>
2024-10-21 12:34:51 +0200

    got the single elements for the date

--- app.js
+++ app.js
@@ -31,3 +31,19 @@ const items = document.querySelectorAll('.deadline-format h4');
 // dichiaro una variabile per stabilire la data di scadenza
 
 let futureDate = new Date(2024, 4, 24, 11, 30, 0);
+
+// estraggo dalla data i valori da inserire nella stringa
+
+const year = futureDate.getFullYear();
+
+const hours = futureDate.getHours();
+
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+
+const date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];
+
